39c85b426867fa1ede9bdb5041f757a42846a6d4
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-08-11 05:22:26 +0300

    relations

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import feature , other , Contact
+from .models import feature , other , Contact , Male , Femele , User , Product , Client , Vedio
     # Register your models here.
 
 admin.site.register(feature)
 admin.site.register(other)
 admin.site.register(Contact)
+admin.site.register(Male)
+admin.site.register(Product)
+admin.site.register(User)
+admin.site.register(Femele)
+admin.site.register(Client)
+admin.site.register(Vedio)
--- chat/models.py
+++ chat/models.py
@@ -17,9 +17,53 @@ class Contact(models.Model):
   comment = models.TextField()
 
 
+#realthionships models
 
+# 1 to 1
+class Femele(models.Model):
+  name_f = models.CharField(max_length=100,null=True)
+  def __str__(self):
+    return self.name_f  
 
-
-
-
+class Male(models.Model):
+  name_m = models.CharField(max_length=100,null=True)
+  girl= models.OneToOneField(Femele, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name_m
     
+
+
+#  1 to meny
+class Product(models.Model):
+  name_m = models.CharField(max_length=100,null=True)
+  def __str__(self):
+    return self.name_m
+    
+
+class User(models.Model):
+  name_f = models.CharField(max_length=100,null=True)
+  products= models.ForeignKey(Product,on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name_f  
+
+
+
+
+# meny to meny
+
+
+
+
+class Vedio(models.Model):
+  title = models.CharField(max_length=100,null=True)
+  def __str__(self):
+    return self.title
+    
+
+class Client(models.Model):
+  name_f = models.CharField(max_length=100,null=True)
+  watch=models.ManyToManyField(Vedio,null=True)
+
+  def __str__(self):
+    return self.name_f  
+
